895736bad5ef9d048b894df168c5a06df1ff9169
Alexander Farley <alexander.s.farley@gmail.com>
2016-01-16 01:46:20 -0500

    Adding sparklines

---
+++ vtc.js
@@ -0,0 +1,28 @@
+ $(function() {
+        /** This code runs when everything has been loaded on the page */
+        /* Inline sparklines take their values from the contents of the tag */
+		
+        $('.inlinesparkline').sparkline('html', {type: 'bar', barColor: 'red'} ); 
+		
+		var values = [55, 5];
+
+// Draw a sparkline for the #sparkline element
+$('#sparkline').sparkline(values, {
+    type: "pie",
+    // Map the offset in the list of values to a name to use in the tooltip
+    tooltipFormat: '{{offset:offset}} ({{percent.1}}%)',
+    tooltipValueLookups: {
+        'offset': {
+            0: 'First',
+            1: 'Second',
+            2: 'Third'
+        }
+    },
+});
+
+
+$('#myCarousel').on('slid.bs.carousel', function () { 
+$.sparkline_display_visible();
+})
+
+    });
